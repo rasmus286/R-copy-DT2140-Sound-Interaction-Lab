822193d1bb30a148f0d9ccefb8efcdf2aacc4574
Rasmus Jansson <rasjan@kth.se>
2025-11-17 13:05:54 +0100

    #bellonshake

--- interaction-1.js
+++ interaction-1.js
@@ -12,7 +12,7 @@ let dspNodeParams = null;
 let jsonParams = null;
 
 // Change here to ("tuono") depending on your wasm file name
-const dspName = "bubble";
+const dspName = "bells";
 const instance = new FaustWasm2ScriptProcessor(dspName);
 
 // output to window or npm package module
@@ -25,7 +25,7 @@ if (typeof module === "undefined") {
 }
 
 // The name should be the same as the WASM file, so change tuono with brass if you use brass.wasm
-bubble.createDSP(audioContext, 1024)
+bells.createDSP(audioContext, 1024)
     .then(node => {
         dspNode = node;
         dspNode.connect(audioContext.destination);
@@ -107,8 +107,8 @@ function playAudio() {
     // them printed on the console of your browser when you load the page)
     // For example if you change to a bell sound, here you could use "/churchBell/gate" instead of
     // "/thunder/rumble".
-    dspNode.setParamValue("/bubble", 1)
-    setTimeout(() => { dspNode.setParamValue("/bubble", 0) }, 100);
+    dspNode.setParamValue("/bells", 1)
+    setTimeout(() => { dspNode.setParamValue("/bells", 0) }, 100);
 }
 
 //==========================================================================================
